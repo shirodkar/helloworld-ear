9b455d49ae7ef7f8be08dd94bd8a48f3e1794268
Sachin Shirodkar <sshirodk@redhat.com>
2026-05-06 01:08:23 -0400

    added import sql

---
+++ backend-api/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into greeting (message) values ('Hello, World!');
+insert into greeting (message) values ('Welcome to Jakarta EE!');
+insert into greeting (message) values ('This is a sample application.');
