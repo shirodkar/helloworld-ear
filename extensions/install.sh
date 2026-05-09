e8772499c5647388b6bc995534a45fb11ffee86d
Sachin Shirodkar <sshirodk@redhat.com>
2026-05-09 13:49:06 -0400

    added install.sh

---
+++ extensions/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -x
+echo "Running $PWD/install.sh"
+injected_dir=$1
+# copy any needed files into the target build.
+cp -rf ${injected_dir} $JBOSS_HOME/extensions
+
+cp /etc/configmaps/jboss-cli/postconfigure.sh $JBOSS_HOME/extensions/postconfigure.sh
