326e7435d37071b9d2485a7e03556688eef308a4
Sachin Shirodkar <sshirodk@redhat.com>
2026-05-14 19:20:39 -0400

    fixed custom module path

--- extensions/install.sh
+++ extensions/install.sh
@@ -5,4 +5,4 @@ injected_dir=$1
 # copy any needed files into the target build.
 cp -rf ${injected_dir} $JBOSS_HOME/extensions
 
-cp $PROJECT_ROOT/custom-module/custom-rolemapper/target/custom-role-mapper.jar $JBOSS_HOME/modules/com/example/helloworld/security/main/
+cp $PROJECT_ROOT/ear/target/module-jars/custom-rolemapper-1.0.0-SNAPSHOT.jar $JBOSS_HOME/modules/com/example/helloworld/security/main/custom-rolemapper.jar
